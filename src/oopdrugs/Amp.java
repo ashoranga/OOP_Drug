607d9789d23b6a720caf01645ec15fa39bf3d4c3
asho8 <m33078@pgim.cmb.ac.lk>
2017-11-23 11:29:57 +0530

    Object oriented programming dm+d

---
+++ src/oopdrugs/Amp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oopdrugs;
+
+/**
+ *
+ * @author MERC-LAB-01
+ */
+public class Amp {
+    Atm atm;
+    Vtm vtm;
+    Vmp vmp;
+    
+}
